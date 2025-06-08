3145c8a753a1ca6e13643a6c97fd25398c943dad
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-06-08 22:58:41 +0530

    feat: Add Debug trait derivation and dbg! macro usage to struct examples

--- struct-examples/src/main.rs
+++ struct-examples/src/main.rs
@@ -1,3 +1,5 @@
+// Adding the attribute to derive the Debug trait
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -19,6 +21,17 @@ fn main() {
         "The area of the rectangle is {} square pixels.",
         area_using_struct(&rect1)
     );
+
+    println!("rect1 is {rect1:#?}");
+
+    // Print out a value using the Debug format using the dbg! macro
+    let scale = 2;
+    let rect2 = Rectangle {
+        width: dbg!(30 * scale),
+        height: 50,
+    };
+
+    dbg!(&rect2);
 }
 
 fn area(dimensions: (u32, u32)) -> u32 {
