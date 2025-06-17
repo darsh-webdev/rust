381167d8736462a5e520d5fb2d908e7958ce7f96
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-06-17 23:12:46 +0530

    feat: Understand pub visibility for struct fields and enum variants

--- modules/src/lib.rs
+++ modules/src/lib.rs
@@ -20,6 +20,16 @@ pub fn eat_at_restaurant() {
 
     // Relative path
     front_of_house::hosting::add_to_waitlist();
+
+    // Order a breakfast in the summer with Rye toast.
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like.
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
+    // meal.seasonal_fruit = String::from("blueberries"); // This will give an error as we're not allowed to see or modify the seasonal fruit
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
 }
 
 mod back_of_house {
@@ -29,6 +39,27 @@ mod back_of_house {
     }
 
     fn cook_order() {}
+
+    // Using pub with struct
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    // Using pub with enum
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
 }
 
 fn deliver_order() {}
