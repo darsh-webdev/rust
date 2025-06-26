935d4b82394c3b3a9606d567da192e59320fdb1e
Darshan Rajput <rajputdarshanashok@gmail.com>
2025-06-26 21:19:06 +0530

    feat: PS-2 Solution - feat: add Pig Latin converter for strings

--- collections-exercises/src/main.rs
+++ collections-exercises/src/main.rs
@@ -30,4 +30,33 @@ fn main() {
     println!("Numbers: {:?}", numbers);
     println!("Median: {}", median);
     println!("Mode: {:?}", mode);
+
+    // Problem Statement 2: Convert strings to pig latin. The first consonant of each word
+    // is moved to the end of the word and ay is added, so first becomes irst-fay.
+    // Words that start with a vowel have hay added to the end instead (apple becomes apple-hay).
+    // Keep in mind the details about UTF-8 encoding!
+
+    let text = "hello world first apple";
+    println!("\nSolution for Problem Statement 2:::::");
+    println!("Text:\n{text}");
+    println!("Pig Latin Text:");
+    for word in text.split_whitespace() {
+        print!("{} ", pig_latin(word));
+    }
+}
+
+// Function for text to pig latin
+fn pig_latin(word: &str) -> String {
+    let vowels = "aeiouAEIOU";
+
+    if let Some(first_char) = word.chars().next() {
+        if vowels.contains(first_char) {
+            format!("{}-hay", word)
+        } else {
+            let rest: String = word.chars().skip(1).collect();
+            format!("{}-{}ay", rest, first_char)
+        }
+    } else {
+        word.to_string()
+    }
 }
